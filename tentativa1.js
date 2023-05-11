6c19e9016ecec139aeac9af34cd02306dc81dbc1
Natalia Alvarez <natalia.fsalvarez@gmail.com>
2023-05-11 20:00:28 -0300

    tentativas com variaveis

---
+++ tentativa1.js
@@ -0,0 +1,21 @@
+// fazendo algumas variaveis para obter respostas.
+
+let x = 2
+let y = 4
+let a = 5
+let b = x + y
+
+let soma = b + x
+console.log(soma)
+
+if (soma >= 7)
+    console.log("Ok!")
+if (soma - a <= 7)
+    console.log("Não")
+console.log(soma - a)
+
+console.log(x, y, a, b)
+
+const d = b * 3
+console.log(`d= ${d}`)
+console.log("d=", d)
