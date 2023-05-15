1d2d52535e240da615757600ffb9c7976597e22d
Natalia Alvarez <natalia.fsalvarez@gmail.com>
2023-05-15 17:46:30 -0300

    colocado uma nova tentativa

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Aqui novamente pra gravar os primeiros comandos")
+
+const x = 3
+const y = 4
+const a = x + 3
+const b = x * 3 - 2
+
+const soma = b + a
+if (soma > 7)
+    console.log("soma > 7, soma=", soma)
+if (soma - b > 2)
+    console.log("soma - b =", soma - b)
